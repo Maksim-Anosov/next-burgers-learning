07f774319a806a741c77d44cdd94172c76afc68e
Maksim Anosov <eastcoastcustoms@yandex.ru>
2024-12-24 18:03:31 +0300

    test4 change

--- src/components/Ingridients/Ingridients.tsx
+++ src/components/Ingridients/Ingridients.tsx
@@ -5,22 +5,32 @@ import { IngridientsUI } from '../ui';
 import { getIngredientsApi } from '@/src/shared/api/burger-api';
 
 export function Ingridients() {
-  const { data } = useQuery({
+  const { data: ingridients, isSuccess, isLoading, error } = useQuery({
     queryKey: ['ingridients'],
     queryFn: getIngredientsApi,
     staleTime: 5 * 60 * 1000
   });
-  
-  const buns = data?.data.filter(item => item.type === 'bun');
-  const mains = data?.data.filter(item => item.type === 'main');
-  const sauces = data?.data.filter(item => item.type === 'sauce');
 
-  return (
-    <IngridientsUI
-      buns={buns ?? []}
-      mains={mains ?? []}
-      sauces={sauces ?? []}
-      addIngridient={() => console.log('addIngridient')}
-    />
-  );
+  if (error) {
+    console.log(error.message);
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isSuccess) {
+    const buns = ingridients.data.filter(item => item.type === 'bun');
+    const mains = ingridients.data.filter(item => item.type === 'main');
+    const sauces = ingridients.data.filter(item => item.type === 'sauce');
+
+    return (
+      <IngridientsUI
+        buns={buns}
+        mains={mains}
+        sauces={sauces}
+        addIngridient={() => console.log('addIngridient')}
+      />
+    );
+  }
 }
