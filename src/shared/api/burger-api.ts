f751115215ab3411e8ec1f043e17a9c36c1a12d9
Maksim Anosov <eastcoastcustoms@yandex.ru>
2024-12-24 18:28:46 +0300

    test7 change

--- src/shared/api/burger-api.ts
+++ src/shared/api/burger-api.ts
@@ -1,6 +1,6 @@
 import { TIngredient } from "@/src/types/types";
 
-const URL = process.env.NEXT_PUBLIC_API_URL;
+// const URL = process.env.NEXT_PUBLIC_API_URL;
 
 const checkResponse = <T>(res: Response): Promise<T> =>
   res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
@@ -15,7 +15,7 @@ type TIngredientsResponse = TServerResponse<{
 
 export async function getIngredientsApi(): Promise<TIngredientsResponse> {
   try {
-    const res = await fetch(`${URL}/ingredients`);
+    const res = await fetch(`https://norma.nomoreparties.space/api/ingredients`);
     return checkResponse<TIngredientsResponse>(res);
   } catch (err) {
     return Promise.reject(err);
