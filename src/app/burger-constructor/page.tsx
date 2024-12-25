53fc086cd33ead560affdaa591cd948b0a70ee1a
Maksim Anosov <eastcoastcustoms@yandex.ru>
2024-12-25 18:12:34 +0300

    ssr fetch ingridients

--- src/app/burger-constructor/page.tsx
+++ src/app/burger-constructor/page.tsx
@@ -1,12 +1,12 @@
 import { Ingridients } from '@/src/components';
 
-export default function Constructor() {
+export default async function Constructor() {
   return (
-      <section className='flex'>
-        <div className='w-1/2 p-4'>
-          <Ingridients />
-        </div>
-        <div className='w-1/2 p-4'>Constructor</div>
-      </section>
+    <section className='flex'>
+      <div className='w-1/2 p-4'>
+        <Ingridients />
+      </div>
+      <div className='w-1/2 p-4'>Constructor</div>
+    </section>
   );
 }
--- src/components/Ingridients/Ingridients.tsx
+++ src/components/Ingridients/Ingridients.tsx
@@ -1,36 +1,12 @@
-'use client';
-
-import { useQuery } from '@tanstack/react-query';
 import { IngridientsUI } from '../ui';
 import { getIngredientsApi } from '@/src/shared/api/burger-api';
 
-export function Ingridients() {
-  const { data: ingridients, isSuccess, isLoading, error } = useQuery({
-    queryKey: ['ingridients'],
-    queryFn: getIngredientsApi,
-    staleTime: 5 * 60 * 1000
-  });
+export async function Ingridients() {
+  const { data } = await getIngredientsApi();
+  
+  const buns = data.filter(item => item.type === 'bun');
+  const mains = data.filter(item => item.type === 'main');
+  const sauces = data.filter(item => item.type === 'sauce');
 
-  if (error) {
-    console.log(error.message);
-  }
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isSuccess) {
-    const buns = ingridients.data.filter(item => item.type === 'bun');
-    const mains = ingridients.data.filter(item => item.type === 'main');
-    const sauces = ingridients.data.filter(item => item.type === 'sauce');
-
-    return (
-      <IngridientsUI
-        buns={buns}
-        mains={mains}
-        sauces={sauces}
-        addIngridient={() => console.log('addIngridient')}
-      />
-    );
-  }
+  return <IngridientsUI buns={buns} mains={mains} sauces={sauces} />;
 }
