eb66c28f3184bf178c53ae76a9a40988b522695f
Maksim Anosov <eastcoastcustoms@yandex.ru>
2025-01-15 15:36:52 +0300

    suspense for constructor page

--- src/app/burger-constructor/page.tsx
+++ src/app/burger-constructor/page.tsx
@@ -1,13 +1,17 @@
 import { BurgerConstructor, Ingridients } from '@/src/components';
+import { IngridientsFallback } from '@/src/components/ui';
+import { Suspense } from 'react';
 export default function Constructor() {
   return (
-    <section className='flex'>
-      <div className='w-1/2 p-4'>
-        <Ingridients />
+    <Suspense fallback={<IngridientsFallback />}>
+    <section className='flex h-full'>
+      <div className='w-1/2 h-full p-4'>
+          <Ingridients />
       </div>
       <div className='w-1/2 p-4'>
         <BurgerConstructor />
       </div>
     </section>
+    </Suspense>
   );
 }
--- src/components/ui/ingridientsUI/index.ts
+++ src/components/ui/ingridientsUI/index.ts
@@ -1,1 +1,2 @@
 export * from './ingridientsUI';
+export * from './ingridientsFallback';
